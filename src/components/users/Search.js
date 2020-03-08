96f14b6366d9be92b239bfde4353d09ce62aa07b
Zuich <zuich-zpb@tut.by>
2020-03-09 00:13:03 +0300

    First commit

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,57 +1,49 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState,useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
-export class Search extends Component {
+const Search= ()=> {
+const githubContext=useContext(GithubContext);
+const alertContext=useContext(AlertContext);
 
-    state={
-text: "",
-    };
+const [text,setText]=useState('');
 
-static propTypes={
-    searchUsers: PropTypes.func.isRequired,
-    clearUsers: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-}
-
-
-    onSubmit=e=>{
+    const onSubmit=e=>{
         e.preventDefault();
-        if(this.state.text===""){
-            this.props.setAlert('Please enter something', 'light');
+        if(text===""){
+           alertContext.setAlert('Please enter something', 'light');
         }else{
-        this.props.searchUsers(this.state.text);
-        this.setState({text:''});
+        githubContext.searchUsers(text);
+        setText("");
             }
         }
 
 
-    onChange=e=>this.setState({[e.target.name]:e.target.value});
-
-
-
-
-    render() {
-    const {showClear, clearUsers}=this.props;
+    const onChange=e=>setText(e.target.value);
 
         return (
             <div>
-            <form  onSubmit={this.onSubmit}  className="form">
+            <form  onSubmit={onSubmit}  className="form">
             <input type="text" 
             name="text" 
             placeholder="search users..." 
-            value={this.state.text}
-            onChange={this.onChange}
+            value={text}
+            onChange={onChange}
             />
             <input type="submit" value="Search" className="btn btn-dark btn-block"/>
             </form>
-            {showClear && <button 
+            {githubContext.users.length>0 && ( <button 
                 className="btn btn-light btn-block" 
-                onClick={clearUsers}>Clear</button> }
+                onClick={githubContext.clearUsers}
+                >
+                Clear
+                </button> 
+            )}
             </div>
         )
     }
-}
+
+
 
 export default Search
 
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,26 +1,22 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect,useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-export class User extends Component {
 
-    componentDidMount(){
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+const User=({match})=>  {
+    const githubContext=useContext(GithubContext);
+    
+    const {getUser,loading,user,repos,getUserRepos}=githubContext;
 
-    static propTypes={
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser:PropTypes.func.isRequired,
-        getUserRepos:PropTypes.func.isRequired
+    useEffect(()=>{
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+   //eslint-disable-next-line
+},[])
 
-    }
-    render() {
-        const {
+const {
 name,
 company,
 avatar_url,
@@ -34,8 +30,7 @@ following,
 public_repos,
 public_gists,
 hireable
-        }=this.props.user;
-        const{ loading,repos }=this.props;
+        }=user;
           if (loading) return <Spinner/>
         return (
             <Fragment>
@@ -103,6 +98,5 @@ Visit Github Profile
             </Fragment>
         )
     }
-}
 
 export default User
